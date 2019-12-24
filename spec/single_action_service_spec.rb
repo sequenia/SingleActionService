90e91029d0c55ab485572b015354f63c588dcfd6
Sergey Bondarenko <sergey.bondarenko@sequenia.com>
2019-12-24 16:20:35 +0700

    [+] SingleActionService, SingleActionService::Base, SingleActionService::Result

--- spec/single_action_service_spec.rb
+++ spec/single_action_service_spec.rb
@@ -3,7 +3,7 @@ RSpec.describe SingleActionService do
     expect(SingleActionService::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it 'kind of SingleActionService' do
+    should be_kind_of(Module)
   end
 end
