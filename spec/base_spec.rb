49f7b1c69a7e6fb3e1ae6cef7d9ab7ff5ac47fc2
Petr Bazov <4ybakut2004@gmail.com>
2021-02-12 12:54:33 +0700

    Added new spect to result and service

--- spec/base_spec.rb
+++ spec/base_spec.rb
@@ -1,21 +1,44 @@
-class DiscountApplicator < SingleActionService::Base
-  ERROR_CODE = 0
+# rubocop:disable Metrics/BlockLength
+RSpec.describe SingleActionService::Base do
+  let(:service) { described_class.new }
 
-  def call(valid)
-    if valid
-      success
-    else
-      error(code: ERROR_CODE)
+  describe '#success' do
+    let(:data) { 'Success Data' }
+    let(:result) { service.send(:success, data) }
+
+    it 'should return result object' do
+      expect(result).to be_a(SingleActionService::Result)
+    end
+
+    it 'should return result with success' do
+      expect(result.success?).to eq(true)
+    end
+
+    it 'should return result with data' do
+      expect(result.data).to eq(data)
+    end
+  end
+
+  describe '#error' do
+    let(:data) { 'Error Data' }
+    let(:error_code) { 1 }
+    let(:result) { service.send(:error, data: data, code: error_code) }
+
+    it 'should return result object' do
+      expect(result).to be_a(SingleActionService::Result)
+    end
+
+    it 'should return result with error' do
+      expect(result.error?).to eq(true)
+    end
+
+    it 'should return result with error code' do
+      expect(result.error_code).to eq(error_code)
+    end
+
+    it 'should return result with data' do
+      expect(result.data).to eq(data)
     end
   end
 end
-
-RSpec.describe SingleActionService::Base do
-  it 'successful action service' do
-    expect(DiscountApplicator.new.call(true).success?).to eq(true)
-  end
-
-  it 'errorful action service' do
-    expect(DiscountApplicator.new.call(false).error?).to eq(true)
-  end
-end
+# rubocop:enable Metrics/BlockLength
